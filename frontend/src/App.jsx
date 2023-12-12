ab971fc51ddf0ee7735f5fe69b58d6e985d8901f
Armando Martinez <alemar.martinez16@gmail.com>
2023-12-12 10:52:11 -0300

    created form - message

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,28 @@
 import io from 'socket.io-client'
+import { useState } from 'react'
 
 const socket = io('/')
 
 function App() {
+  const [message, setMessage] = useState('')
+
+  const handleSbmit = (e) => {
+    e.preventDefault()
+    console.log(message)
+  }
+
   return (
-    <div>Hello World</div>
+    <div>
+
+      <form onSubmit={handleSbmit}>
+
+        <input type="text"  placeholder='write your message ...'
+          onChange = {(e) => setMessage(e.target.value)}
+        />
+        <button>send</button>
+      </form>
+
+    </div>
   )
 }
 
