839028083f5e7042d0c43b0d26f617828ac7e2c8
Armando Martinez <alemar.martinez16@gmail.com>
2023-12-12 10:21:38 -0300

    connection server-frontend by socket.io

--- server/index.js
+++ server/index.js
@@ -2,14 +2,18 @@ import express from 'express';
 import http from 'http';
 import { Server as SocketServer } from 'socket.io';
 
-
 const app = express();
 const server = http.createServer(app);
-const io = new SocketServer(server);
+const io = new SocketServer(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+    }
+});
 
-io.on ('conection', socket => {
-    console.log ('Client connected');
-})
+io.on('connection', socket => {
+    console.log('Client connected');
+});
 
-app.listen(3000);
-console.log('Server on port', 3000)
+server.listen(3000, () => {
+    console.log('Server on port 3000');
+});
