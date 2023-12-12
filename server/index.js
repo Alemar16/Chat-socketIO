43036783b63ace2d1370c06cf7265d672a9e21cd
Armando Martinez <alemar.martinez16@gmail.com>
2023-12-12 10:25:20 -0300

    configutation connection from vite.config.js

--- server/index.js
+++ server/index.js
@@ -4,11 +4,7 @@ import { Server as SocketServer } from 'socket.io';
 
 const app = express();
 const server = http.createServer(app);
-const io = new SocketServer(server, {
-    cors: {
-        origin: 'http://localhost:5173',
-    }
-});
+const io = new SocketServer(server);
 
 io.on('connection', socket => {
     console.log('Client connected');
