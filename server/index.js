3ee2e1dcfe6b8174d1f09d3f464c8e3f73be324f
Armando Martinez <alemar.martinez16@gmail.com>
2023-12-12 09:54:36 -0300

    integrated socket.io

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import http from 'http';
+import { Server as SocketServer } from 'socket.io';
 
 
 const app = express();
+const server = http.createServer(app);
+const io = new SocketServer(server);
+
 
 app.listen(3000);
 console.log('Server on port', 3000)
