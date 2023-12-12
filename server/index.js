de04cef3ce73491ca2689163cf4ed018a99675f9
Armando Martinez <alemar.martinez16@gmail.com>
2023-12-12 10:03:12 -0300

    conection server to frontend

--- server/index.js
+++ server/index.js
@@ -7,6 +7,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new SocketServer(server);
 
+io.on ('conection', socket => {
+    console.log ('Client connected');
+})
 
 app.listen(3000);
 console.log('Server on port', 3000)
