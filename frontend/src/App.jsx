db3418c932b5175b442bf3d15d0d059b4bb79050
Armando Martinez <alemar.martinez16@gmail.com>
2023-12-13 14:47:16 -0300

    Change PC + news styles chat

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,55 +7,67 @@ function App() {
   const [message, setMessage] = useState(""); //para escribir el mensaje
   const [messages, setMessages] = useState([]); //para mostrar los mensajes(array de mensajes)
 
+ 
+
   //para enviar el mensaje al backend
   const handleSbmit = (e) => {
     e.preventDefault();
     //console.log(message)
-    const newMessage = {//para mostrar el mensaje en el mismo chat del que envia el id
+    const newMessage = {
+      //para mostrar el mensaje en el mismo chat del que envia el id
       body: message,
       from: "Me",
-    }
-    setMessages([...messages, newMessage]);//para mostrar el mensaje en el mismo chat del que envia
-    socket.emit("message", message);//para mandar el mensaje
+    };
+    setMessages([...messages, newMessage]); //para mostrar el mensaje en el mismo chat del que envia
+    socket.emit("message", message); //para mandar el mensaje
+   
   };
-
 
   //se mantendrá escuchando el evento message del backend para mostrar los mensajes en tiempo real
   useEffect(() => {
-    socket.on("message", reciveMessage)//recivo el mensaje y lo muestro
-      //console.log(message); 
+    socket.on("message", reciveMessage); //recivo el mensaje y lo muestro
+    //console.log(message);
     return () => {
-      socket.off("message", reciveMessage);//muestro el mensaje solo una vez y lo actualizo
-    }
+      socket.off("message", reciveMessage); //muestro el mensaje solo una vez y lo actualizo
+    };
   }, []);
 
-  const reciveMessage = (message)=> 
-  setMessages(state => [...state, message])
-    
-  
+  const reciveMessage = (message) =>
+    setMessages((state) => [...state, message]);
 
   return (
-    <div className="h-screen bg-zinc-900 text-white flex items-center justify-center">
+    <div className="h-screen bg-zinc-200 text-white flex items-center justify-center">
       <form onSubmit={handleSbmit} className="bg-zinc-800 p-10 rounded">
-        <h1 className="text-3xl font-bold mb-5">Chat React</h1>
-        <input
-          type="text"
-          placeholder="write your message ..."
-          className="border-2 border-zinc-600 p-2 w-full rounded text-black"
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <button>send</button>
-      <ul>
-        {messages.map((message, index) => (
-          <li key={index} className={
-            `my-2 p-2 table text-sm rounded-md ${
-              message.from === "Me" ? "bg-sky-900 ml-auto" : "bg-sky-600"
-            }`
-          }>
-            <span className="font-bold text-xs text-slate-900 block">{message.from}</span> <span className=" text-md font-bold shadow">{message.body}</span>
+        <div>
+          <img src="/icons8-chat-100.png" alt="" />
+          <h1 className="text-3xl font-bold mb-5">Chat Socket.io</h1>
+        </div>
+
+        <div className="mb-5 flex gap-3">
+          <input
+            type="text"
+            placeholder="write your message ..."
+            className="border-2 border-zinc-600 p-2 w-full rounded text-black"
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button className="btn hover:bg-blue-800">Send</button>
+        </div>
+
+        <ul className="max-h-60 overflow-y-auto">
+          {messages.map((message, index) => (
+            <li
+              key={index}
+              className={`my-2 p-2 table text-sm rounded-md ${
+                message.from === "Me" ? "bg-sky-900 ml-auto" : "bg-sky-600"
+              }`}
+            >
+              <span className="font-bold text-xs text-slate-900 block">
+                {message.from}
+              </span>{" "}
+              <span className=" text-md font-bold shadow">{message.body}</span>
             </li>
-        ))}
-      </ul>
+          ))}
+        </ul>
       </form>
     </div>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,28 @@
+
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".btn": {
+          padding: ".5rem 1rem",
+          borderRadius: ".25rem",
+          fontSize: "1rem",
+          fontWeight: "600",
+          backgroundColor: "#0070f3", // Puedes ajustar el color base aquí
+          color: "#ffffff",
+          cursor: "pointer",
+          transition: "background-color 0.3s ease", // Agregamos una transición suave al cambio de color
+        },
+      };
+
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    },
+  ],
 };
