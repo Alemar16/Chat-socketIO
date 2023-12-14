14e42f809a2e99eed4e0a67a25cfbe50cfb52220
Armando Martinez <alemar.martinez16@gmail.com>
2023-12-14 04:01:11 -0300

    use "form" form date-fns. configuration

--- frontend/src/components/ListMessageComponent/ListMessageComponent.jsx
+++ frontend/src/components/ListMessageComponent/ListMessageComponent.jsx
@@ -1,30 +1,39 @@
 import PropTypes from "prop-types";
+import { format } from "date-fns";
 
 const ListMessageComponent = ({ messages }) => {
   return (
     <div className="max-w-sm w-full">
-      {/* Establece el ancho máximo y ancho completo */}
-      <ul className="max-h-60 p-4 mb-5 overflow-y-auto scrollbar-none  ">
-        {messages.length > 0 ? (
-          messages.map((message, index) => (
-            <li
-              key={index}
-              className={`my-2 p-2 table text-sm rounded-md shadow-lg shadow-indigo-900/80 ${
-                message.from === "Me" ? "bg-purple-900 ml-auto" : "bg-blue-400"
-              }`}
-            >
-              <span className="text-xs text-slate-900 block font-sans">
-                {message.from}
-              </span>{" "}
-              <span className="text-lg font-poppins text-justify text-zinc-50 font-bold ">{message.body}</span>
-            </li>
-          ))
-        ) : (
-          <li className="p-2 text-sm text-gray-800 flex items-center justify-center">
-            No messages yet.
-          </li>
+      <ul className="max-h-60 p-4 mb-5 overflow-y-auto scrollbar-none">
+  {messages.length > 0 ? (
+    messages.map((message, index) => (
+      <li
+        key={index}
+        className={`my-2 p-2 table text-sm rounded-md shadow-lg shadow-indigo-900/80 relative ${
+          message.from === "Me" ? "bg-purple-900 ml-auto" : "bg-blue-400"
+        }`}
+        style={{ paddingBottom: '1.2rem', minWidth: '100px' }} 
+      >
+        <span className="text-xs text-slate-900 block font-sans">
+          {message.from}
+        </span>{" "}
+        <span className="text-lg font-poppins text-justify text-zinc-50 font-bold">
+          {message.body}
+        </span>
+        {message.timestamp && (
+          <span className="text-xs text-zinc-300 absolute bottom-1 right-1">
+            {format(new Date(message.timestamp), "hh:mm a", { timeZone: "auto" })}
+          </span>
         )}
-      </ul>
+      </li>
+    ))
+  ) : (
+    <li className="p-2 text-sm text-gray-800 flex items-center justify-center">
+      No messages yet.
+    </li>
+  )}
+</ul>
+
     </div>
   );
 };
@@ -34,6 +43,7 @@ ListMessageComponent.propTypes = {
     PropTypes.shape({
       body: PropTypes.string.isRequired,
       from: PropTypes.string.isRequired,
+      timestamp: PropTypes.string.isRequired, // Asegúrate de que timestamp esté definido como una cadena
     })
   ).isRequired,
 };
