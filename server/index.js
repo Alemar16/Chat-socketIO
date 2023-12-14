de2aa61f6e4f6c3803d42140e6d23d9020807e46
Armando Martinez <alemar.martinez16@gmail.com>
2023-12-14 03:59:54 -0300

    add currentTime with new Date()

--- server/index.js
+++ server/index.js
@@ -1,23 +1,35 @@
-import express from 'express';
-import http from 'http';
-import { Server as SocketServer } from 'socket.io';
+import { time } from "console";
+import express from "express";
+import http from "http";
+import { Server as SocketServer } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
 const io = new SocketServer(server);
 
-io.on('connection', socket => {
-    console.log(socket.id);
+io.on("connection", (socket) => {
+  console.log(socket.id);
 
-    socket.on('message',(body) =>{//escuchar el mensaje
-        console.log(body);
-        socket.broadcast.emit('message', {
-            body,
-            from: socket.id.slice(6)//para mostrar solo el id del cliente que envia
-        });//reponde el mensaje
-    })
+  socket.on("message", (body) => {
+    //escuchar el mensaje
+    console.log(body);
+
+    const currentTime = new Date().toLocaleTimeString();
+
+    // Emite una confirmación al remitente con la hora
+    socket.emit("messageConfirmation", {
+      body: `Mensaje recibido correctamente a las ${currentTime}.`,
+      from: "Server",
+    });
+
+    socket.broadcast.emit("message", {
+      body,
+      from: socket.id.slice(6), //para mostrar solo el id del cliente que envia
+      time: currentTime,
+    }); //reponde el mensaje
+  });
 });
 
 server.listen(3000, () => {
-    console.log('Server on port 3000');
+  console.log("Server on port 3000");
 });
