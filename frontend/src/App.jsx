762f5bff142263d460237f691420b76ad21d6b40
Armando Martinez <alemar.martinez16@gmail.com>
2023-12-15 13:59:20 -0300

    Startup is configured with username

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import io from "socket.io-client";
 import { useState, useEffect } from "react";
 import FormComponent from "./components/FormComponent/FormComponent";
 import ListMessageComponent from "./components/ListMessageComponent/ListMessageComponent";
+import LoginComponent from "./components/LoginComponent/LoginComponent";
 
 const socket = io("/");
 
 function App() {
   const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState("");
 
   useEffect(() => {
     socket.on("message", reciveMessage);
@@ -14,6 +16,11 @@ function App() {
       socket.off("message", reciveMessage);
     };
   }, []);
+
+  const handleLogin = (username) => {
+    setUsername(username);
+    socket.emit("login", username);
+  }
 
   const reciveMessage = (message) => {
     const newMessage = {
@@ -25,7 +32,7 @@ function App() {
     setMessages((state) => [newMessage, ...state]);
   };
   
-    const handleSbmit = (message) => {
+    const handleSubmit = (message) => {
       const newMessage = {
         body: message,
         from: "Me",
@@ -36,16 +43,18 @@ function App() {
     };
     
 
-  return (
-    <div
-      className="h-screen bg-[url('./assets/image-background.jpg')] bg-cover bg-center text-white flex flex-col items-center justify-center"
-    >
-      <div className="backdrop-saturate-125 bg-white/20 rounded-2xl shadow-lg shadow-slate-900/60 ">
-        <FormComponent onSubmit={handleSbmit} />
-        <ListMessageComponent messages={messages} />
+    return (
+      <div className="h-screen bg-[url('./assets/image-background.jpg')] bg-cover bg-center text-white flex flex-col items-center justify-center">
+        <div className="backdrop-saturate-125 bg-white/20 rounded-2xl shadow-lg shadow-slate-900/60 ">
+          {username ? (
+            <FormComponent onSubmit={handleSubmit} username={username} />
+          ) : (
+            <LoginComponent onLogin={handleLogin} />
+          )}
+          <ListMessageComponent messages={messages} />
+        </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default App;
