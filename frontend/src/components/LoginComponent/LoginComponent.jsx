eba0bdcd784100659b352cf417352ca6c1c6ed5c
Armando Martinez <alemar.martinez16@gmail.com>
2023-12-15 18:56:47 -0300

    Some styles are added to the login

--- frontend/src/components/LoginComponent/LoginComponent.jsx
+++ frontend/src/components/LoginComponent/LoginComponent.jsx
@@ -10,20 +10,26 @@ const LoginComponent = ({ onLogin }) => {
   };
 
   return (
-    <div className="flex gap-3">
-      <input
-        placeholder="Enter your username..."
-        className="border-2 border-zinc-600 p-2 w-full rounded text-black bg-opacity-50 focus:bg-white focus:outline-none focus:bg-opacity-100 shadow-lg focus:shadow-indigo-900 transition-all duration-300"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <button
-        type="button"
-        onClick={handleLogin}
-        className="btn hover:bg-blue-800 shadow-lg shadow-indigo-900/80 cursor-pointer"
-      >
-        Login
-      </button>
+    <div>
+      <div className="flex items-center shadow-lg p-2">
+        <img src="/icons8-chat-100.png" alt="Logo-Chat" />
+        <h1 className="text-xl font-bold mb-5">Chat Socket.io</h1>
+      </div>
+      <div className="flex items-center shadow-lg p-2">
+        <input
+          placeholder="Enter your username..."
+          className="border-none focus:outline-none bg-white text-gray-700 rounded-l-lg p-2 w-full"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button
+          type="button"
+          onClick={handleLogin}
+          className="bg-blue-500 text-white px-4 py-2 rounded-r-lg hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue"
+        >
+          Login
+        </button>
+      </div>
     </div>
   );
 };
