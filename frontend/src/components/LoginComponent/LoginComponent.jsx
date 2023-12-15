e323f8d6ec711d6ecb1c082966caa123b02424fe
Armando Martinez <alemar.martinez16@gmail.com>
2023-12-15 13:56:20 -0300

    login is created

---
+++ frontend/src/components/LoginComponent/LoginComponent.jsx
@@ -0,0 +1,32 @@
+// LoginComponent.jsx
+import { useState } from "react";
+
+const LoginComponent = ({ onLogin }) => {
+  const [username, setUsername] = useState("");
+
+  const handleLogin = () => {
+    console.log("Login button clicked with username:", username);
+    onLogin(username);
+  };
+
+  return (
+    <div className="flex gap-3">
+      <input
+        placeholder="Enter your username..."
+        className="border-2 border-zinc-600 p-2 w-full rounded text-black bg-opacity-50 focus:bg-white focus:outline-none focus:bg-opacity-100 shadow-lg focus:shadow-indigo-900 transition-all duration-300"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <button
+        type="button"
+        onClick={handleLogin}
+        className="btn hover:bg-blue-800 shadow-lg shadow-indigo-900/80 cursor-pointer"
+      >
+        Login
+      </button>
+    </div>
+  );
+};
+
+export default LoginComponent;
+
