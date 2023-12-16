9def10b0393d854dac1c0131c8e10608cde8bb99
Armando Martinez <alemar.martinez16@gmail.com>
2023-12-16 11:12:06 -0300

    run fronten dist fron server with resolve (path)

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import http from "http";
 import { Server as SocketServer } from "socket.io";
+import { resolve } from "path";
 
 const app = express();
 const server = http.createServer(app);
 const io = new SocketServer(server);
 
 const users = {};
+//console.log(resolve('frontend/dist'));
+
+app.use(express.static(resolve('frontend/dist')));//desde server se levanta el front.
 
 io.on("connection", (socket) => {
   console.log(`${socket.id} connected`);
