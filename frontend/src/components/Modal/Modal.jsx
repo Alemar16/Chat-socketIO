d8caa141e8beea8f2c252edefb669fc2d285dd42
Armando Martinez <alemar.martinez16@gmail.com>
2024-02-12 01:59:11 -0300

    "Refactored Modal component for improved styling and readability"
    Explanation:
    
    Updated the Modal component to enhance its visual presentation and readability.
    Replaced the plain text title with styled spans for "Flash" and "Chat" to apply custom font styles, colors, and text shadows for better visual appeal.
    Applied the "Boogaloo" font family to both "Flash" and "Chat" spans for consistency and aesthetic appeal.
    Adjusted the HTML structure and styles for better organization and readability, including consistent font colors and styling for instructional text.
    Modified the confirmation button style to use a purple color theme, matching the color scheme of the application, and added a shadow effect for visual depth.
    Improved the modal background by applying a semi-transparent gray color to provide better contrast with the content while maintaining visibility.
    Refactored padding and spacing for a more balanced and aesthetically pleasing layout.

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -7,36 +7,33 @@ const Modal = () => {
   useEffect(() => {
     const modal = Swal.fire({
       title:
-        'Welcome to <br> <span class="text-blue-800">Flash</span><span class="text-purple-500">Chat</span>',
-      titleStyle: {
-        fontSize: "24px",
-        fontWeight: "bold",
-        color: "#000000",
-      },
+      '<span style="color: #852CA5; text-shadow: 2px 2px 4px #000; font-family: Boogaloo, cursive;">Flash</span><span style="color: #4CCFF1;text-shadow: 2px 2px 4px #000; font-family: Boogaloo, cursive;">Chat</span>',
       imageUrl: "/icons8-chat-100.png",
       html: `
-        <p><strong>How to Use:</strong></p><br>
+        <p style="color: black;"><strong>How to Use:</strong></p><br>
         <ol>
-          <li>Enter your name and click "<span class="font-bold">Login</span>"<br> or <br> "Enter as <span class="font-bold">Anonymous</span>".</li>
+          <li style="color: black;>Enter your name and click "<span class="font-bold">Login</span>"<br> or <br> "Enter as <span class="font-bold">Anonymous</span>".</li>
           <br>
-          <li>Share this link:</li>
+          <li style="color: black;">Share this link:</li>
           <br>
           <button id="enlace" style="background-color: #ffffff; border: 1px solid #3182ce; color: #3182ce; padding: 8px 16px; font-size: 16px; font-weight: bold; cursor: pointer; border-radius: 4px; transition: background-color 0.3s, color 0.3s;" class="text-blue-800 cursor-pointer font-bold">Link</button>
           <br>
-          <li class="text-red-900 font-bold text-2xl">Start Chatting and Enjoy!</li> 
+          <li class="text-red-900 font-bold text-3xl"><span style="color: #4CCFF1; text-shadow: 2px 2px 4px #000; font-family: Boogaloo, cursive;">Start<BR> Chatting and Enjoy!</span></li> 
+         
         </ol>
       `,
       customClass: {
         modal: "rounded-lg shadow-xxl",
-        title: "text-center",
+        title: "text-center text-6xl mt-[-4rem]",
         imageUrl: "mx-auto mt-3",
         htmlContainer: "p-4",
         confirmButton:
-          "bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue",
+        "bg-purple-900 text-white px-4 py-2 rounded-lg shadow-md hover:bg-purple-800 focus:outline-none focus:shadow-outline-blue",
       },
       width: 500,
-      padding: "1em",
-      background: "#fe9393",
+      padding: "0.5em",
+      background: "rgba(180, 180, 180, 0.7)", 
+
       didOpen: (modalElement) => {
         modalElement.querySelector("#enlace").addEventListener("click", () => {
           navigator.clipboard.writeText(
