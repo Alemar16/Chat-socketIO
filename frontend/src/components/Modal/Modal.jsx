59e6cfbc337b88af229a3672ced6ee43886453d0
Armando Martinez <alemar.martinez16@gmail.com>
2024-02-11 22:03:52 -0300

    Refactor modal component to utilize React hooks and integrate SweetAlert2
    - Updated Modal.jsx to use React's useState and useEffect hooks for managing component state and side effects respectively.
    - Integrated SweetAlert2 library to display a modal popup with instructions and functionalities for the chat application.
    - Reorganized and enhanced the modal content to provide clear instructions and improve user experience.

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -7,7 +7,7 @@ const Modal = () => {
   useEffect(() => {
     const modal = Swal.fire({
       title:
-        'Bienvenido a <br> <span class="text-blue-800">Flash</span><span class="text-purple-500">Chat</span>',
+        'Welcome to <br> <span class="text-blue-800">Flash</span><span class="text-purple-500">Chat</span>',
       titleStyle: {
         fontSize: "24px",
         fontWeight: "bold",
@@ -15,15 +15,15 @@ const Modal = () => {
       },
       imageUrl: "/icons8-chat-100.png",
       html: `
-        <p><strong>Para chatear o crear un grupo sigue estos sencillos pasos:</strong></p><br>
+        <p><strong>How to Use:</strong></p><br>
         <ol>
-          <li>Ingresa tu nombre y haz clic en "Login" o "Ingresar como Anónimo".</li>
+          <li>Enter your name and click "<span class="font-bold">Login</span>"<br> or <br> "Enter as <span class="font-bold">Anonymous</span>".</li>
           <br>
-          <li>Comparte este enlace:</li>
+          <li>Share this link:</li>
           <br>
-          <button id="enlace" style="background-color: #ffffff; border: 1px solid #3182ce; color: #3182ce; padding: 8px 16px; font-size: 16px; font-weight: bold; cursor: pointer; border-radius: 4px; transition: background-color 0.3s, color 0.3s;" class="text-blue-800 cursor-pointer font-bold">Enlace</button>
+          <button id="enlace" style="background-color: #ffffff; border: 1px solid #3182ce; color: #3182ce; padding: 8px 16px; font-size: 16px; font-weight: bold; cursor: pointer; border-radius: 4px; transition: background-color 0.3s, color 0.3s;" class="text-blue-800 cursor-pointer font-bold">Link</button>
           <br>
-          <li class="text-red-900 font-bold text-2xl">¡Diviértete chateando!</li> 
+          <li class="text-red-900 font-bold text-2xl">Start Chatting and Enjoy!</li> 
         </ol>
       `,
       customClass: {
@@ -35,17 +35,17 @@ const Modal = () => {
           "bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue",
       },
       width: 500,
-      padding: "2em",
-      background: "#ffffff",
+      padding: "1em",
+      background: "#fe9393",
       didOpen: (modalElement) => {
         modalElement.querySelector("#enlace").addEventListener("click", () => {
           navigator.clipboard.writeText(
             "https://chat-socketio-n9to.onrender.com/"
           );
           setCopied(true);
-          // Cambiar el texto del botón a "Copiado"
-          modalElement.querySelector("#enlace").innerText = "Copiado";
-          // Cambiar el color de fondo y del texto del botón a verde
+          // Change button text to "Copied"
+          modalElement.querySelector("#enlace").innerText = "Copied";
+          // Change button background and text color to green
           modalElement.querySelector("#enlace").style.backgroundColor = "#4CAF50";
           modalElement.querySelector("#enlace").style.color = "#ffffff";
         });
