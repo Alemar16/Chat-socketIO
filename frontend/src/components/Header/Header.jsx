0c9cb4c86c51cbe2871d0e81e8597879f86a7a69
Armando Martinez <alemar.martinez16@gmail.com>
2024-02-12 00:43:32 -0300

    Enhance Header component with custom font and styling
    The Header component has been updated to use the "Boogaloo" font family for the title text. Additionally, the title text now features a custom color scheme and text shadow to improve visual appeal. The word "Flash" now has a purple color with a dark stroke effect for added emphasis, while the word "Chat" remains blue. These changes enhance the overall design and branding of the Chat Socket.io application.

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 function Header() {
   return (
     <div className="text-center">
-          <img
-            src="/icons8-chat-100.png"
-            alt="Logo-Chat"
-            className="w-20 h-20 mx-auto mb-1"
-          />
-          <h1 className="text-3xl font-bold mb-5">Chat Socket.io</h1>
-        </div>
-  )
+      <img
+        src="/icons8-chat-100.png"
+        alt="Logo-Chat"
+        className="w-20 h-20 mx-auto mb-1"
+      />
+      {/* Aplica la fuente Boogaloo al elemento <h1> */}
+      <h1 className="text-3xl font-bold mb-5" style={{ fontFamily: 'Boogaloo, cursive', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
+      <span style={{ color: '#852CA5', textStroke: '2px #000' }}>Flash</span><span style={{ color: '#4CCFF1'  }}>Chat</span>
+      </h1>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
