ea87e4fcf3320439b46dfc019b6c9d242e2d0507
Armando Martinez <alemar.martinez16@gmail.com>
2024-03-04 20:59:29 -0300

    feat: Enhance ConnectedUsersList component
    This commit improves the ConnectedUsersList component by adding additional styles and functionality.
    
    Added import statement for activoIcon and "../../App.css" to include necessary styles and icons.
    Adjusted the container size and styling to fit a smaller width and height, with rounded corners.
    Implemented overflow-y-auto to enable vertical scrolling when the list of connected users exceeds the container height.
    Updated the list style to remove the disc bullet and added padding to the left.
    Added an icon next to each user's name to indicate their status.
    Applied a truncate class to ensure long usernames are displayed properly and prevent overflow.
    These changes enhance the visual appearance and usability of the ConnectedUsersList component, providing a more polished user experience.

--- frontend/src/components/ConnectedUsersList/ConnectedUsersList.jsx
+++ frontend/src/components/ConnectedUsersList/ConnectedUsersList.jsx
@@ -1,12 +1,22 @@
 import PropTypes from "prop-types";
+import activoIcon from "../../assets/icons/activo.png";
+import "../../App.css";
+
 
 const ConnectedUsersList = ({ users }) => {
   return (
-    <div className="max-w-md w-370 p-4 mt-5 flex flex-col items- backdrop-saturate-125 bg-white/20 rounded-2xl shadow-lg shadow-slate-900/60 ">
-      <h2 className="text-xl font-bold mb-3">Usuarios Conectados:</h2>
-      <ul className="list-disc pl-5">
+    <div className="max-w-xs w-52 h-60 p-4 mt-5 mx-auto flex flex-col items-center backdrop-saturate-125 bg-white/20 rounded-md shadow-lg shadow-slate-900/60 overflow-y-auto overflow-x-hidden">
+      {/* Resto del código */}
+      <h2 className="text-xl font-bold mb-3 text-center ">
+        Usuarios Conectados
+      </h2>
+      <ul className="list-disc pl-2">
         {users.map((user, index) => (
-          <li key={index}>{user.username}</li>
+          <li key={index} className="flex items-center">
+            {" "}
+            <img src={activoIcon} alt="" className="w-4 h-4 mr-2" />{" "}
+            <span className="text-md truncate">{user.username}</span>
+          </li>
         ))}
       </ul>
     </div>
