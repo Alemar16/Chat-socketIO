1e01949d90820e653db42731f10f913b6e4111af
Armando Martinez <alemar.martinez16@gmail.com>
2024-03-05 01:18:52 -0300

    feat: Add functionality to show/hide users and change icon
    - Added state for controlling visibility of user list
    - Changed icon dynamically based on visibility state
    - Added close icon to close user list
    - Improved styling and hover effect for better user experience
    This commit includes the following changes:
    
    Added state to control the visibility of the user list.
    Dynamically changed the icon displayed based on the visibility state.
    Added a close icon to allow users to close the user list.
    Enhanced styling and added a hover effect to improve the user experience.

--- frontend/src/components/Buttons/ButtonShowUsers.jsx
+++ frontend/src/components/Buttons/ButtonShowUsers.jsx
@@ -1,26 +1,44 @@
-// ButtonShowUsers.jsx
 import { useState } from "react";
 import PropTypes from "prop-types";
 import usersIcon from "../../assets/icons/users-icon.svg";
+import usersIcon2 from "../../assets/icons/users-icon2.svg";
+import closeIcon from "../../assets/icons/close-icon.svg";
 
-export const ButtonShowUsers = ({ onShowUsers }) => {
+export const ButtonShowUsers = ({ onShowUsers, connectedUsers }) => {
   const [show, setShow] = useState(false);
 
   const handleShowUsers = () => {
     setShow(!show);
-    onShowUsers(!show); 
+    onShowUsers(!show);
   };
 
   return (
-    <img
-      src={usersIcon}
-      alt="Show Users"
-      className="h-16 w-16 cursor-pointer"
-      onClick={handleShowUsers}
-    />
+    <div className="relative">
+      <img
+        src={show ? usersIcon2 : usersIcon} // Cambio de icono según el estado 'show'
+        alt="Show Users"
+        className="h-16 w-16 cursor-pointer hover:scale-110"
+        onClick={handleShowUsers}
+      />
+      {show && (
+        <img
+          src={closeIcon}
+          alt="Close"
+          className="absolute top-0 right-0 -mt-2 -mr-2 cursor-pointer"
+          style={{ width: "24px", height: "24px" }}
+          onClick={handleShowUsers}
+        />
+      )}
+      {connectedUsers.length > 0 && (
+        <span className="absolute bg-green-500 text-white rounded-full w-6 h-6 flex items-center justify-center text-xs top-8 -right-5 animate-pulse">
+          {connectedUsers.length > 99 ? "+99" : connectedUsers.length}
+        </span>
+      )}
+    </div>
   );
 };
 
 ButtonShowUsers.propTypes = {
   onShowUsers: PropTypes.func.isRequired,
-}
+  connectedUsers: PropTypes.array.isRequired,
+};
