cb47df0f072fc647775138200097398db4487bab
Armando Martinez <alemar.martinez16@gmail.com>
2024-03-04 21:45:26 -0300

    feat: Improve ConnectedUsersList component layout and functionality
    Refactored the ConnectedUsersList component to enhance its layout and functionality. Changes include:
    
    - Added a counter to display the number of connected users.
    - Rearranged the layout to display the counter next to the title.
    - Adjusted styles to ensure proper visibility and spacing of elements.
    - Updated PropTypes to reflect changes in component structure.
    
    These changes improve the overall user experience and make the component more informative and visually appealing.

--- frontend/src/components/ConnectedUsersList/ConnectedUsersList.jsx
+++ frontend/src/components/ConnectedUsersList/ConnectedUsersList.jsx
@@ -2,23 +2,31 @@ import PropTypes from "prop-types";
 import activoIcon from "../../assets/icons/activo.png";
 import "../../App.css";
 
-
 const ConnectedUsersList = ({ users }) => {
+  // Calcula el número de usuarios conectados
+  const connectedCount = users.length;
+
   return (
-    <div className="max-w-xs w-52 h-60 p-4 mt-5 mx-auto flex flex-col items-center backdrop-saturate-125 bg-white/20 rounded-md shadow-lg shadow-slate-900/60 overflow-y-auto overflow-x-hidden">
-      {/* Resto del código */}
-      <h2 className="text-xl font-bold mb-3 text-center ">
-        Usuarios Conectados
-      </h2>
-      <ul className="list-disc pl-2">
-        {users.map((user, index) => (
-          <li key={index} className="flex items-center">
-            {" "}
-            <img src={activoIcon} alt="" className="w-4 h-4 mr-2" />{" "}
-            <span className="text-md truncate">{user.username}</span>
-          </li>
-        ))}
-      </ul>
+    <div className="max-w-xs w-55 h-60 p-4 mt-5 mx-auto flex flex-col items-center backdrop-saturate-125 bg-white/20 rounded-md shadow-lg shadow-slate-900/60 overflow-hidden">
+      {/* Título y contador */}
+      <div className="flex items-center justify-between gap-2 mb-3 backdrop-saturate-125 bg-white/20 rounded-md shadow-lg shadow-slate-900/60 py-2 px-4">
+        <h2 className="text-xl font-bold text-center">Usuarios Conectados</h2>
+        <span className="bg-green-500 text-white rounded-full shadow-lg shadow-slate-900/60 w-10 h-10 flex items-center justify-center text-lg">
+          {connectedCount}
+        </span>
+      </div>
+      {/* Contenido desplazable */}
+      <div className="overflow-y-auto h-full">
+        <ul className="list-disc pr-4">
+          {/* Lista de usuarios */}
+          {users.map((user, index) => (
+            <li key={index} className="flex items-center">
+              <img src={activoIcon} alt="" className="w-4 h-4 mr-2" />
+              <span className="text-md truncate">{user.username}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
