bfb4851333869ced26ac801d8d45d2ddb4303a48
Armando Martinez <alemar.martinez16@gmail.com>
2024-03-29 13:14:55 -0300

    feat: Add error handling for empty message in FormComponent
    - Import ButtonSend component for sending messages.
    - Add errorMessage and errorVisible states to manage error handling.
    - Implement logic to display error message and hide it after 3 seconds.
    - Validate message in handleSubmit function to prevent empty submissions.
    - Add conditional classes to form for displaying red border on error.
    - Adjust error message placement and style for better visibility.
    
    This commit improves user experience by providing clear feedback when attempting to send an empty message, preventing accidental submissions.

--- frontend/src/components/FormComponent/FormComponent.jsx
+++ frontend/src/components/FormComponent/FormComponent.jsx
@@ -1,8 +1,7 @@
-// FormComponent.jsx
-
 import io from "socket.io-client";
 import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import ButtonSend from "../Buttons/ButtonSend";
 
 import ConnectedUsersList from "../ConnectedUsersList/ConnectedUsersList";
 import { ButtonShowUsers } from "../Buttons/ButtonShowUsers";
@@ -15,6 +14,8 @@ const FormComponent = ({ onSubmit, username }) => {
   const [message, setMessage] = useState("");
   const [showConnectedUsersModal, setShowConnectedUsersModal] = useState(false);
   const [connectedUsers, setConnectedUsers] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [errorVisible, setErrorVisible] = useState(false); // State para controlar la visibilidad del error
 
   useEffect(() => {
     socket.on("users", updateConnectedUsers);
@@ -23,10 +24,27 @@ const FormComponent = ({ onSubmit, username }) => {
     };
   }, []);
 
+  useEffect(() => {
+    if (errorMessage) {
+      setErrorVisible(true); 
+      const timer = setTimeout(() => {
+        setErrorMessage("");
+        setErrorVisible(false); 
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [errorMessage]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    onSubmit(message);
-    setMessage("");
+    if (message.trim() === "") {
+      setErrorMessage("Please write your message.");
+    } else {
+      onSubmit(message);
+      setMessage("");
+      setErrorMessage("");
+    }
   };
 
   const updateConnectedUsers = (users) => {
@@ -41,30 +59,30 @@ const FormComponent = ({ onSubmit, username }) => {
           <div>
             <ButtonShowUsers
               onShowUsers={setShowConnectedUsersModal}
-              connectedUsers={connectedUsers} // Pasar connectedUsers como prop
+              connectedUsers={connectedUsers} 
             />
           </div>
         </div>
       )}
       <form
         onSubmit={handleSubmit}
-        className="border-2 border-zinc-600 rounded p-1"
+        className={`rounded-md bg-white p-1 mb-1 ${
+          errorVisible ? "border-2 border-red-500" : ""
+        }`}
       >
         <div className="flex gap-2">
           <input
             placeholder="Write your message..."
-            className="border-2 border-zinc-600 p-2 w-full rounded text-black bg-opacity-50 focus:bg-white focus:outline-none focus:bg-opacity-100 shadow-lg focus:shadow-indigo-900 transition-all duration-300"
+            className="p-2 w-full rounded text-black bg-opacity-50 focus:bg-white focus:outline-none focus:bg-opacity-100 "
             value={message}
             onChange={(e) => setMessage(e.target.value)}
           />
-          <button
-            className="btn hover:bg-blue-800 shadow-lg shadow-indigo-900/80 cursor-pointer"
-            type="submit"
-          >
-            Send
-          </button>
+          <ButtonSend onClick={handleSubmit} />
         </div>
       </form>
+        {errorMessage && (
+          <div className="text-red-600 text-sm ml-2 -mt-1">{errorMessage}</div>
+        )}
       {showConnectedUsersModal && (
         <div className="modal absolute top-40 left-0 z-50 w-full h-full flex items-center justify-center">
           <ConnectedUsersList users={connectedUsers} />
