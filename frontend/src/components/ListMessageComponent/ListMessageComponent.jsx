dec4433b2ceb925a996511d1ad985e9864eef747
Armando Martinez <alemar.martinez16@gmail.com>
2024-03-29 16:52:55 -0300

    Refactor ListMessageComponent styles and structure
    Description:
    Refactored the ListMessageComponent to improve code structure and enhance visual consistency. Adjusted styles and removed unnecessary div containers for a cleaner and more streamlined component.
    
    Changes Made:
    
    Removed redundant div containers surrounding the ListMessageComponent.
    Adjusted component structure to eliminate unnecessary nesting and improve code readability.
    Refactored styles to use Tailwind CSS classes for consistent styling and reduced inline styling.
    Updated the container div to use a backdrop-blur effect for a visually appealing background.
    Justification:
    This refactor simplifies the structure of the ListMessageComponent, making it more maintainable and easier to understand. By removing unnecessary divs and consolidating styles with Tailwind CSS, the component's codebase becomes cleaner and more coherent. Additionally, the use of a backdrop-blur effect enhances the component's visual presentation, contributing to a more polished user interface.

--- frontend/src/components/ListMessageComponent/ListMessageComponent.jsx
+++ frontend/src/components/ListMessageComponent/ListMessageComponent.jsx
@@ -1,61 +1,66 @@
 import PropTypes from "prop-types";
 import { format } from "date-fns";
+
 const ListMessageComponent = ({ messages }) => {
   return (
-    <div className="max-w-[384px] w-full">
-      {messages.length > 0 ? (
-        <ul className="max-h-[339.13px] overflow-y-auto p-3 mb-5 scrollbar-none">
-          {messages.map((message, index) => (
-            <li
-              key={index}
-              className={`my-2 p-2 table text-sm rounded-md shadow-lg shadow-indigo-900/80 relative ${
-                message.from === "Me" ? "bg-purple-700 ml-auto" : "bg-blue-400"
-              }`}
-              style={{
-                paddingBottom: "1.2rem",
-                minWidth: "100px",
-                borderRadius:
+    <div className="max-w-[384px] w-full relative">
+      <div className="backdrop-blur-xl bg-white/40 rounded-lg shadow-lg shadow-slate-900/60">
+        {messages.length > 0 ? (
+          <ul className="max-h-[223px] overflow-y-auto p-3 mb-5">
+            {messages.map((message, index) => (
+              <li
+                key={index}
+                className={`my-2 p-2 table text-sm rounded-md shadow-lg shadow-indigo-900/80 relative ${
                   message.from === "Me"
-                    ? "18px 18px 0 18px"
-                    : "18px 18px 18px 0px",
-              }}
-            >
-              {/* Contenido del mensaje */}
-              <span className="text-xs text-slate-900 block font-sans font-semibold">
-                {message.from}
-              </span>{" "}
-              <span
-                className="text-sm font-poppins text-justify text-zinc-50"
-                style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)" }}
-              >
-                {message.body}
-              </span>
-              {/* Marca de tiempo del mensaje */}
-              {message.timestamp && (
-                <span
-                  className={` ${
+                    ? "bg-purple-700 ml-auto"
+                    : "bg-blue-400"
+                }`}
+                style={{
+                  paddingBottom: "1.2rem",
+                  minWidth: "100px",
+                  borderRadius:
                     message.from === "Me"
-                      ? "text-zinc-300"
-                      : "text-zinc-800 px-2"
-                  } absolute bottom-1 right-1`}
-                  style={{
-                    textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
-                    fontSize: "9px",
-                  }}
+                      ? "18px 18px 0 18px"
+                      : "18px 18px 18px 0px",
+                }}
+              >
+                {/* Contenido del mensaje */}
+                <span className="text-xs text-slate-900 block font-sans font-semibold">
+                  {message.from}
+                </span>{" "}
+                <span
+                  className="text-sm font-poppins text-justify text-zinc-50"
+                  style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)" }}
                 >
-                  {format(new Date(message.timestamp), "hh:mm a", {
-                    timeZone: "auto",
-                  })}
+                  {message.body}
                 </span>
-              )}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p className="p-2 text-sm text-gray-800 flex items-center justify-center">
-          No messages yet.
-        </p>
-      )}
+                {/* Marca de tiempo del mensaje */}
+                {message.timestamp && (
+                  <span
+                    className={` ${
+                      message.from === "Me"
+                        ? "text-zinc-300"
+                        : "text-zinc-800 px-2"
+                    } absolute bottom-1 right-1`}
+                    style={{
+                      textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+                      fontSize: "9px",
+                    }}
+                  >
+                    {format(new Date(message.timestamp), "hh:mm a", {
+                      timeZone: "auto",
+                    })}
+                  </span>
+                )}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className="p-2 text-sm text-gray-800 flex items-center justify-center">
+            No messages yet.
+          </p>
+        )}
+      </div>
     </div>
   );
 };
